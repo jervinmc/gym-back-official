adef56b35fe8d38bb00304c4d251166bba7d28ca
Jervin Macalawa <jervinmac@yahoo.com>
2023-05-06 22:39:13 +0800

    done

--- app/app/urls.py
+++ app/app/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from users.views import Login,GetUserView,Signup,GetUser
 from transaction.views import TransactionNotif
+from book.views import CheckBook
 from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -20,6 +21,7 @@ path('api/v1/admin/', admin.site.urls),
     path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/auth/user/', GetUserView.as_view(), name='auth_data'),
     path('api/v1/login/', Login.as_view(), name='token_refresh'),
+    path('api/v1/check-book/', CheckBook.as_view(), name='token_refresh'),
     path('api/v1/get-user/', GetUser.as_view(), name='token_refresh'),
     path('api/v1/transaction-notif/', TransactionNotif.as_view(), name='token_refresh'),
     path('api/v1/users/', include('users.urls')),
--- app/book/views.py
+++ app/book/views.py
@@ -5,6 +5,8 @@ from .serializers import BookSerializer
 from rest_framework import filters
 from rest_framework import permissions
 from rest_framework.response import Response
+from datetime import datetime
+
 from rest_framework import status, viewsets
 from users.models import User
 from users.serializers import GetUserSerializer
@@ -16,5 +18,23 @@ class BookView(viewsets.ModelViewSet):
     queryset=Book.objects.all()
     permission_classes = (permissions.AllowAny, )
     serializer_class=BookSerializer
+
+
+
+class CheckBook(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny, )
+    def post(self,request):
+        res = request.data
+        date = res.get('date')
+        date1 = datetime.strptime(date[:10], "%Y-%m-%d")
+        # formatted_date = date1.strftime("YYYY-MM-DD")
+        instance = Book.objects.filter(appointment_date__date=date1)
+        # instance = Book.objects.all()
+        serializer_response = BookSerializer(instance,many=True)
+        total_response = len(serializer_response.data)
+        print(total_response)
+        return Response(status=status.HTTP_200_OK,data=total_response)
+
+
 
 
